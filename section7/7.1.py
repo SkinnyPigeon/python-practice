7ba308febec87d488a3973d864fc55e030f667ea
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2018-02-13 21:49:01 +0000

    different ways to import things

---
+++ section7/7.1.py
@@ -0,0 +1,17 @@
+import math
+
+print( math.pi )
+print( math.cos(math.pi))
+
+from math import pi
+
+print( pi )
+
+from math import *
+
+print( sin(pi) )
+print(factorial(6))
+
+from math import factorial as f
+
+print( f(6) )
