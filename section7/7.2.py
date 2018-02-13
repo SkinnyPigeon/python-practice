44653fa5c9bd851d1ad45c376f1e1af5522f8e4c
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2018-02-13 22:03:00 +0000

    WTF. The server is super awesome. Cannot believe how much functionality there is built in. Apparently emails etc

---
+++ section7/7.2.py
@@ -0,0 +1,15 @@
+# Looking at the python documentation
+
+import shutil
+# shutil.copyfile( 'hello', 'test' )
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
